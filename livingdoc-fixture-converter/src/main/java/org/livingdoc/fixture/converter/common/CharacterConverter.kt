3d2bd189edceb60ff30e6c27ad9892fec350d77e
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-06-09 11:02:42 +0200

    added type converter for Character

---
+++ livingdoc-fixture-converter/src/main/java/org/livingdoc/fixture/converter/common/CharacterConverter.kt
@@ -0,0 +1,17 @@
+package org.livingdoc.fixture.converter.common
+
+import org.livingdoc.fixture.api.converter.ConversionException
+import org.livingdoc.fixture.api.converter.TypeConverter
+import java.lang.reflect.AnnotatedElement
+
+
+open class CharacterConverter : TypeConverter<Char> {
+
+    override fun convert(value: String, element: AnnotatedElement?): Char {
+        if (value.length != 1) {
+            throw ConversionException("not a char value: '$value'")
+        }
+        return value[0]
+    }
+
+}
