ab257650966c90fe49eac2d10a1060c62a74a210
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2017-07-03 18:28:44 +0200

    added @Testable to document binding in order to enable IDE support
    The dependency to the JUnit Platform is compile time only and therefore not transitive. No one HAS to have JUnit on the class path in order to use @ExecutableDocument, but if our JUnit Engine is used, classes annotated with it will be recognized by the IDE as "testable" and allow an execution.

--- livingdoc-fixture-api/src/main/java/org/livingdoc/fixture/api/binding/ExecutableDocument.java
+++ livingdoc-fixture-api/src/main/java/org/livingdoc/fixture/api/binding/ExecutableDocument.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.platform.commons.annotation.Testable;
 
+
+@Testable
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExecutableDocument {
