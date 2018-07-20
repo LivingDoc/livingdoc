d30e3a8576f75c3cdbf9a29c094362b04cbbe2dd
Peter Kleimann <peter.kleimann@novatec-gmbh.de>
2018-07-20 17:06:02 +0200

    Add module livingdoc-sample with Calculator example

---
+++ livingdoc-sample/src/main/kotlin/org/livingdoc/example/Calculator.kt
@@ -0,0 +1,21 @@
+package org.livingdoc.example
+
+class Calculator {
+
+    fun sum(a: Float, b: Float): Float {
+        return a + b
+    }
+
+    fun diff(a: Float, b: Float): Float {
+        return a - b
+    }
+
+    fun multiply(a: Float, b: Float): Float {
+        return a * b
+    }
+
+    fun divide(a: Float, b: Float): Float {
+        return a / b
+    }
+
+}
